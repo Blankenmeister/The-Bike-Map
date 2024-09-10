d7c264cd1a416c483052be322f4c285ab57b3888
Sonia <sonia.blankenmeister@gmail.com>
2024-09-10 14:19:18 +0200

    feat : add favicon folder

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -1,6 +1,30 @@
 <?php
 
+namespace src\Repositories;
+
+use src\Models\User;
+use PDO;
+use src\Models\Database;
+
+class UserRepository 
+{
+    private $DB;
+
+    public function __construct() 
+    {
+        $database = new Database;
+        $this->DB = $database->getDB();
 
 
+        require_once __DIR__ . '/../../config.php';
+    }
+
+
+    
+    
+
+
+
+}
 
 
