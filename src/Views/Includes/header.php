c00d81d0af31e243b61d86be7de7591aea913b58
Sonia <sonia.blankenmeister@gmail.com>
2024-10-08 11:17:11 +0200

    layout, add list page and back button

--- src/Views/Includes/header.php
+++ src/Views/Includes/header.php
@@ -21,8 +21,7 @@
   <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/style/styleHeader.css' ?>">
   <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/style/styleHome.css' ?>">
   <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/style/styleFooter.css' ?>">
-  <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/style/mobile.css' ?>">
-
+  <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/style/styleMapList.css' ?>">
 
   <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/bootstrap/css/bootstrap.css' ?>">
   <link rel="stylesheet" href="<?= Domain . HOME_URL . 'assets/bootstrap/css/bootstrap-grid.css' ?>">
@@ -76,6 +75,7 @@
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
               </svg>
             </a>
+            <a href="<?= Domain . HOME_URL ?>signout">Déconnexion</a>
           </li>
         </ul>
 
@@ -85,10 +85,6 @@
     </div>
   </nav>
 
-
-
-    <a href="<?= Domain . HOME_URL ?>signout">Déconnexion</a>
-    <a href="<?= Domain . HOME_URL ?>signin">Connexion</a>
 
 
 
--- src/Views/Pages/mapList.php
+++ src/Views/Pages/mapList.php
@@ -2,7 +2,106 @@
 
 
 
-<?php echo 'je suis dans la page map list'; ?>
+<div class="container mt-5">
+
+  <a href="<?= Domain . HOME_URL ?>">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#22A737" class="bi bi-arrow-left" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+    </svg>
+  </a>
+
+
+  <div class="row mb-5">
+    <div class="col-sm-12 col-md-6 ">
+      <label for="level" class="form-label"></label>
+      <select id="level" name="Id_level" class="form-select rounded-pill py-3" required>
+        <option selected>NIVEAU</option>
+        <option value="1">Familial</option>
+        <option value="2">Sportif</option>
+        <option value="3">Expert</option>
+      </select>
+    </div>
+
+    <div class="col-sm-12 col-md-6">
+      <label for="type" class="form-label"></label>
+      <select id="type" name="Id_type" class="form-select rounded-pill py-3" required>
+        <option selected>PRATIQUE</option>
+        <option value="1">Route</option>
+        <option value="2">Gravel</option>
+        <option value="3">VTT</option>
+      </select>
+    </div>
+
+  </div>
+</div>
+</div>
+
+<div class="container mb-5">
+  <div class="row g-4">
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <a class="card-link" href="<?= Domain . HOME_URL ?>mapDetail">
+        <div class="card">
+          <img class="card-img-top" src="assets/image/bali.jpg" />
+          <div class="card-text p-4">
+            <div class="type-heart d-flex justify-content-between align-items-end mb-1">
+              <p class="card-type m-0">VTT</p>
+              <div class="circle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#22A737" class="bi bi-heart" viewBox="0 0 16 16">
+                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                </svg>
+              </div>
+            </div>
+            <h5 class="card-name">Le Fort de Comboire</h5>
+            <p class="card-level">Niveau familial</p>
+
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <a class="card-link" href="<?= Domain . HOME_URL ?>mapDetail">
+        <div class="card">
+          <img class="card-img-top" src="assets/image/bali.jpg" />
+          <div class="card-text p-4">
+            <div class="type-heart d-flex justify-content-between align-items-end mb-1">
+              <p class="card-type m-0">VTT</p>
+              <div class="circle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#22A737" class="bi bi-heart" viewBox="0 0 16 16">
+                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                </svg>
+              </div>
+            </div>
+            <h5 class="card-name">Le Fort de Comboire</h5>
+            <p class="card-level">Niveau familial</p>
+
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <a class="card-link" href="<?= Domain . HOME_URL ?>mapDetail">
+        <div class="card">
+          <img class="card-img-top" src="assets/image/bali.jpg" alt="photographie pour illustrer un endroit du parcours" />
+          <div class="card-text p-4">
+            <div class="type-heart d-flex justify-content-between align-items-end mb-1">
+              <p class="card-type m-0">VTT</p>
+              <div class="circle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#22A737" class="bi bi-heart" viewBox="0 0 16 16">
+                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                </svg>
+              </div>
+            </div>
+            <h5 class="card-name">Le Fort de Comboire</h5>
+            <p class="card-level">Niveau familial</p>
+
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
 
 
 
